16ef2e631ec0efd2a126e49109340762cffe5272
marijnland <m.aarts25@gmail.com>
2022-03-23 12:15:31 +0100

    Added working kwh counters

--- Team_Charge_Interface/Mainwindow.py
+++ Team_Charge_Interface/Mainwindow.py
@@ -80,24 +80,26 @@ class MainWindow():
         self.canvas_text_power_right = self.mainwindow_canvas.create_text(600,227, text = str(round((self.spot2.power/1000),1))+ " kW", font=("Helvetica",16, "bold"), fill= "white", state="hidden")
 
         #Time info, hidden by default
-        self.canvas_text_time_left = self.mainwindow_canvas.create_text(213,247, text = "Placeholder", font=("Helvetica",15), fill="white", state="hidden")
-        self.canvas_text_time_right = self.mainwindow_canvas.create_text(600,247, text = "Placeholder", font=("Helvetica",15), fill="white", state="hidden")
+        self.canvas_text_time_left = self.mainwindow_canvas.create_text(213,246, text = "Placeholder", font=("Helvetica",15), fill="white", state="hidden")
+        self.canvas_text_time_right = self.mainwindow_canvas.create_text(600,246, text = "Placeholder", font=("Helvetica",15), fill="white", state="hidden")
 
         #Present card
         self.canvas_image_present_card_left = self.mainwindow_canvas.create_image(207,400, image=self.tk_present_card_image)
         self.canvas_image_present_card_right = self.mainwindow_canvas.create_image(594,400, image=self.tk_present_card_image)
 
-        #Kwh counter
+        #Kwh counter, hidden by default
         self.canvas_image_kwh_counter_left = self.mainwindow_canvas.create_image(207,400, image=self.tk_kwh_counter_image, state="hidden")
         self.canvas_image_kwh_counter_right = self.mainwindow_canvas.create_image(594,400, image=self.tk_kwh_counter_image, state="hidden")
 
+        #kwh text info, hidden by default
+        self.canvas_text_kwh_left = self.mainwindow_canvas.create_text(182,400, text= "placeholder", font=("Helvetica",16, "bold"), fill= "white", state="hidden")
+        self.canvas_text_kwh_right = self.mainwindow_canvas.create_text(569,400, text= "placeholder", font=("Helvetica",16, "bold"), fill= "white", state="hidden")
+
         #Activate frame drawer
         self.mainwindow_canvas.after(50,self.draw_frame)
+        self.mainwindow_canvas.after(100, self.update_active_elements)
 
-
-
-    def draw_frame(self):
-        #172 degrees pointer (+86 to -86)
+    def update_active_elements(self):
         if (self.spot1.is_active):
             self.mainwindow_canvas.itemconfig(self.canvas_image_pointer_left, state="normal")
             self.mainwindow_canvas.itemconfig(self.canvas_text_power_left, state="normal")
@@ -105,6 +107,7 @@ class MainWindow():
             self.mainwindow_canvas.itemconfig(self.canvas_text_time_left, state="normal")
             self.mainwindow_canvas.itemconfig(self.canvas_image_present_card_left, state="hidden")
             self.mainwindow_canvas.itemconfig(self.canvas_image_kwh_counter_left, state="normal")
+            self.mainwindow_canvas.itemconfig(self.canvas_text_kwh_left, state="normal")
         else:
             self.mainwindow_canvas.itemconfig(self.canvas_image_pointer_left, state="hidden")
             self.mainwindow_canvas.itemconfig(self.canvas_text_power_left, state="hidden")
@@ -112,7 +115,7 @@ class MainWindow():
             self.mainwindow_canvas.itemconfig(self.canvas_text_time_left, state="hidden")
             self.mainwindow_canvas.itemconfig(self.canvas_image_present_card_left, state="normal")
             self.mainwindow_canvas.itemconfig(self.canvas_image_kwh_counter_left, state="hidden")
-
+            self.mainwindow_canvas.itemconfig(self.canvas_text_kwh_left, state="hidden")
 
         if (self.spot2.is_active):
             self.mainwindow_canvas.itemconfig(self.canvas_image_pointer_right, state="normal")
@@ -121,6 +124,7 @@ class MainWindow():
             self.mainwindow_canvas.itemconfig(self.canvas_text_time_right, state="normal")
             self.mainwindow_canvas.itemconfig(self.canvas_image_present_card_right, state="hidden")
             self.mainwindow_canvas.itemconfig(self.canvas_image_kwh_counter_right, state="normal")
+            self.mainwindow_canvas.itemconfig(self.canvas_text_kwh_right, state="normal")
         else:
             self.mainwindow_canvas.itemconfig(self.canvas_image_pointer_right, state="hidden")
             self.mainwindow_canvas.itemconfig(self.canvas_text_power_right, state="hidden")
@@ -128,8 +132,14 @@ class MainWindow():
             self.mainwindow_canvas.itemconfig(self.canvas_text_time_right, state="hidden")
             self.mainwindow_canvas.itemconfig(self.canvas_image_present_card_right, state="normal")
             self.mainwindow_canvas.itemconfig(self.canvas_image_kwh_counter_right, state="hidden")
+            self.mainwindow_canvas.itemconfig(self.canvas_text_kwh_right, state="hidden")
 
-            
+
+        self.mainwindow_canvas.after(500, self.update_active_elements)
+
+    def draw_frame(self):
+        #172 degrees pointer (+86 to -86)
+        
         #Left spot (1)
         self.angle = 86 - ((self.spot1.power/12000) * 172) + random.randint(-1, 1)
         self.temp_rotate_image = self.pointer_image.rotate(self.angle, expand=True)
@@ -137,7 +147,9 @@ class MainWindow():
         self.mainwindow_canvas.itemconfig(self.canvas_image_pointer_left, image=self.rotated_pointer_image_left)
         self.mainwindow_canvas.itemconfig(self.canvas_text_power_left, text = str(round((self.spot1.power/1000),1)) + " kW")
         self.mainwindow_canvas.itemconfig(self.canvas_text_time_left, text = str(int((time.time() - self.spot1.start_time) / 60)) + ":" + str(int((time.time() - self.spot1.start_time) % 60)))
-       
+        self.mainwindow_canvas.itemconfig(self.canvas_text_kwh_left, text = str(int(self.spot1.kwh/100)) + "    " + str(int((self.spot1.kwh/10)%10)) + "    " + str(int(self.spot1.kwh%10)) + "         "  + str(int((self.spot1.kwh*10)%10)) + "    " + str(int((self.spot1.kwh*100)%10)))
+        
+                                          
         #Right spot (2)
         self.angle = 86 - ((self.spot2.power/12000) * 172) + random.randint(-1, 1)
         self.temp_rotate_image = self.pointer_image.rotate(self.angle, expand=True)
@@ -145,6 +157,7 @@ class MainWindow():
         self.mainwindow_canvas.itemconfig(self.canvas_image_pointer_right, image=self.rotated_pointer_image_right)
         self.mainwindow_canvas.itemconfig(self.canvas_text_power_right, text = str(round((self.spot2.power/1000),1)) + " kW")
         self.mainwindow_canvas.itemconfig(self.canvas_text_time_right, text = str(int((time.time() - self.spot2.start_time) / 60)) + ":" + str(int((time.time() - self.spot2.start_time) % 60)))
+        self.mainwindow_canvas.itemconfig(self.canvas_text_kwh_right, text = str(int(self.spot2.kwh/100)) + "    " + str(int((self.spot2.kwh/10)%10)) + "    " + str(int(self.spot2.kwh%10)) + "         " + str(int((self.spot2.kwh*10)%10)) + "    " + str(int((self.spot2.kwh*100)%10)))
 
 
         self.mainwindow_canvas.after(500, self.draw_frame)
